0ba03c542394eac3523fdf72008d2c8d226983b6
Chloe <Valicit@gmail.com>
2017-09-06 11:59:15 -0500

    Sprites Added

---
+++ Protoment/Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Element
+{
+    Fire,
+    Water,
+    Wood,
+    Electric,
+    Light,
+    Dark
+}
+
+public class Unit
+{
+    //This class is the base class for every unit in the game. If it can be placed in combat at all, it's got one of these. Even if it can't be placed in combat, it might have one of these.
+
+    //These are all the units current stats.
+    public int id = 1;
+    public int level = 1;
+    public long exp = 0;
+    public string uName = "Name";
+    public string job = "Job";
+    public Element uElement = Element.Fire;
+    public long mHP = 100;
+    public long cHP = 100;
+    public long STR = 10;
+    public long DEF = 10;
+    public long INT = 10;
+    public long SPR = 10;
+    public long DEX = 10;
+    public long AGI = 10;
+    public float Crit = 0.05f;
+    public float CritDMG = 1.5f;
+    public float Speed = 1;
+
+    //These are the units bonus stats.
+    public long bHP = 100;
+    public long bSTR = 10;
+    public long bDEF = 10;
+    public long bINT = 10;
+    public long bSPR = 10;
+    public long bDEX = 10;
+    public long bAGI = 10;
+}
